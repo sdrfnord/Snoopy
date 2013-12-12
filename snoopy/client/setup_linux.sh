8dfa589b9a062ba53fca9f6cead675fb55e4dc60
Samuel Cozannet <samuelc@doremitechno.org>
2013-12-12 08:48:22 +0100

    adding aircrack-ng and psmisc

--- snoopy/client/setup_linux.sh
+++ snoopy/client/setup_linux.sh
@@ -20,12 +20,7 @@ echo "+-----------------------------------------------------------------------+
 #Set current path in config
 sd=$(cd $(dirname "$0"); pwd)
 
-apt-get install -y dnsmasq
-apt-get install -y tshark
-apt-get install -y openvpn
-apt-get install -y rsync
-apt-get install -y netcat
-apt-get install -y macchanger
+apt-get install -y dnsmasq tshark openvpn rsync netcat macchanger aircrack-ng psmisc traceroute
 
 /etc/init.d/ssh start
 
