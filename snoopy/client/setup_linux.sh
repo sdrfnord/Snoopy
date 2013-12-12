5e480dffbe0650ab9398ed1b76b8c2c3d44d925e
Samuel Cozannet <samuelc@doremitechno.org>
2013-12-12 12:27:00 +0100

    adding other packets for linux

--- snoopy/client/setup_linux.sh
+++ snoopy/client/setup_linux.sh
@@ -13,14 +13,15 @@ echo "+-----------------------------------------------------------------------+
 +-----------------------------------------------------------------------+
 + This script has been tested on a BackTrack5 installation. I'll try	+
 + install the following packages anway:					+
-+ -dnsmasq, tshark, openvpn, rsync, netcat, macchanger			+
++ -dnsmasq, tshark, openvpn, rsync, netcat, macchanger, psmisc, iptables+
++  aircrack-ng                                                          +
 +-----------------------------------------------------------------------+
 "
 
 #Set current path in config
 sd=$(cd $(dirname "$0"); pwd)
 
-apt-get install -y dnsmasq tshark openvpn rsync netcat macchanger aircrack-ng psmisc traceroute
+apt-get install -y dnsmasq tshark openvpn rsync netcat macchanger aircrack-ng psmisc traceroute iptables
 
 /etc/init.d/ssh start
 
